afcde5a94d2d938489c4bac2fe3a1990c681e22b
Michael <cuencamich@hotmail.com>
2021-08-04 21:16:09 -0400

    Committing updated Heroku required files V5

--- patient_doctors/wsgi.py
+++ patient_doctors/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'patient_doctors.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'patient_doctors.settings')
 
 application = get_wsgi_application()
